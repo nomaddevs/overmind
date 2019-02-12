62d51bc05895b888df8064a4cbb8f60b8c885975
tmunson <tmunson@wsu.edu>
2019-02-11 18:08:56 -0800

    update main

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from absl import app
 def main(unused_argv):
 	agent = zerg()
 	map = 'AbyssalReef'
+	agent_iface_fmt = features.AgentInterfaceFormat(feature_dimensions = features.Dimensions(screen = 84, minimap = 64), use_feature_units = True)
+	player_setup = [sc2_env.Agent(sc2_env.Race.zerg), sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.very_easy)]
 	try:
 		while True:
 			with sc2_env.SC2Env(
 				map_name = map,
-				players = [sc2_env.Agent(sc2_env.Race.zerg), sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.very_easy)],
-				agent_interface_format=features.AgentInterfaceFormat(feature_dimensions = features.Dimensions(screen = 84, minimap = 64), use_feature_units = True),
+				players = player_setup,
+				agent_interface_format=agent_iface_fmt,
 				step_mul = 16,
 				game_steps_per_episode = 0,
 				visualize = False) as env:
 					agent.setup(env.observation_spec(), env.action_spec())
-
 					timesteps = env.reset()
 					agent.reset()
-
 					while True:
 						step_actions = [agent.step(timesteps[0])]
 						if timesteps[0].last():
